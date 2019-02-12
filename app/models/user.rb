202970f71b413d107994c704855f94ad10010b02
Linda Ho <linda.ho@shoplineapp.com>
2019-02-12 18:48:41 +0800

    2. Add validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+	validates :first_name, :last_name, presence: true
+	validates :gender, inclusion: { in: %w(male female other), message: "%{value} is not valid." }
+    validates :age, numericality: { greater_than: 0 }
+
+
 	def address
 		{country: country, address_1: address_1, address_2: address_2}
 	end
